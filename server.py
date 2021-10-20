9de3ff7fa09987c597c5ff37f7cb4d17e9c9dd92
thowitz <46631044+thowitz@users.noreply.github.com>
2021-10-20 18:21:46 +0100

    Added ability to read chain from file

--- server.py
+++ server.py
@@ -5,10 +5,20 @@ from block import Block
 from transaction import Transaction
 from wallet import Wallet
 from node import Node
+import json
+import requests
 
 blockchain = BlockChain.getInstance()
 wallet = Wallet.getInstance()
 node = Node.getInstance()
+
+savedChainFile = open("chain.json")
+savedChain = json.load(savedChainFile)
+
+if savedChain:
+    blockchain.chain = savedChain
+
+blockchain.createGenesis()
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
