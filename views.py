3e93dd0d32f1a719b24428193ded2c480a5a485f
thowitz <46631044+thowitz@users.noreply.github.com>
2021-10-20 22:03:38 +0100

    Added new route and removed duplication

--- views.py
+++ views.py
@@ -9,19 +9,32 @@ from transaction import Transaction
 blockchain = BlockChain.getInstance()
 node = Node.getInstance()
 
+
+def standardResponse(conditional):
+    if conditional:
+        return True, 200
+    else:
+        return False, 400
+
+
 views = Blueprint(__name__, "views")
 
 
-@views.route("/chain")
+@views.route("/chain/current")
 def returnChain():
-    return json.dumps(blockchain.chain)
+    return json.dumps(blockchain.chain), 200
 
 
-@views.route("/nodes")
+@views.route("/chain/new", methods=["POST"])
+def newChain():
+    return standardResponse(blockchain.processProspectiveChain())
+
+
+@views.route("/nodes/current")
 def returnNodes():
-    return json.dumps(node.nodes)
+    return json.dumps(node.nodes), 200
 
 
 @views.route("/newBlock", methods=["POST"])
 def newBlock():
-    return blockchain.processProspectiveBlock()
+    return standardResponse(blockchain.processProspectiveBlock())
