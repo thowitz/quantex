e23f0532e9de79f488e8b9e77672c56c98a76206
thowitz <46631044+thowitz@users.noreply.github.com>
2021-10-20 15:36:33 +0100

    Added node and new block routes

--- views.py
+++ views.py
@@ -1,8 +1,13 @@
 from flask import Blueprint
 from blockchain import BlockChain
+from block import Block
+from node import Node
 import json
 
+from transaction import Transaction
+
 blockchain = BlockChain.getInstance()
+node = Node.getInstance()
 
 views = Blueprint(__name__, "views")
 
@@ -12,6 +17,22 @@ def returnChain():
     return json.dumps(blockchain.chain)
 
 
-# @views.route("/nodes")
-# def returnNodes():
-#     return json.dumps(node.nodes)
+@views.route("/nodes")
+def returnNodes():
+    return json.dumps(node.nodes)
+
+
+@views.route("/newBlock", methods=["POST"])
+def newBlock():
+    # todo check for correct validator public key
+
+    if not Block.validateBlocks(newBlock, blockchain.chain[-1]):
+        return False
+    elif not Transaction.validateTransactions(newBlock.transactionList):
+        return False
+
+    blockchain.appendBlock(newBlock)
+
+    # todo notify other validators
+
+    return True
