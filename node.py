ac9c6af8811b2d153975e76760b4cbb0afc3f57a
thowitz <46631044+thowitz@users.noreply.github.com>
2021-10-21 17:36:17 +0100

    Added processing for prospective new nodes

--- node.py
+++ node.py
@@ -11,15 +11,34 @@ class Node:
     def reset(cls):
         cls._instance = None
 
-    def __init__(self, instanceExists=False):
+    def __init__(self, instanceExists: bool = False):
         if not instanceExists:
             raise RuntimeError(
                 f"{self.__class__.__name__} is a singleton, use the getInstance class method."
             )
 
         nodes = []
-        file = open("nodes.txt")
-        for node in file.readlines():
+        savedNodesFile = open("nodes.txt")
+        for node in savedNodesFile.readlines():
             nodes.append(node.strip())
 
+        savedNodesFile.close()
+
         self.nodes = nodes
+
+    def processProspectiveNodes(self, prospectiveNodes: list):
+        newNodes = []
+
+        savedNodesFile = open("nodes.txt", "a")
+
+        for node in prospectiveNodes:
+            if not node in self.nodes:
+                newNodes.append(node)
+                savedNodesFile.write(f"{node}/n")
+
+        savedNodesFile.close()
+
+        if newNodes:
+            self.nodes.append(newNodes)
+            return True
+        return False
