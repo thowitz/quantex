65a87dc416fda07a13d75091ee0561fd31c2a73d
thowitz <timjhowitz@gmail.com>
2022-01-15 19:27:47 +0000

    Made mining easier for my slow laptop

--- block.py
+++ block.py
@@ -35,7 +35,7 @@ class Block:
         hash.update(guess)
         attempt = hash.hexdigest()
 
-        return attempt[:4] == "0000"
+        return attempt[:2] == "00"
 
     def fromDict(self, blockDict: dict):
         # verify enough of the data points exist in the dict
