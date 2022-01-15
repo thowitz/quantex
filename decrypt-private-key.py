1f33c99ecb3f71c6c4c5f6ca8fd7d69e2f5a57f0
thowitz <timjhowitz@gmail.com>
2022-01-15 21:32:38 +0000

    Added printing of public key

--- decrypt-private-key.py
+++ decrypt-private-key.py
@@ -5,4 +5,7 @@ privateKeyPassword = input("Please enter your private key password: ")
 wallet = Wallet.getInstance()
 
 if wallet.checkExistingPrivateKey():
+    print("\nPrivate key:")
     print(wallet.readPrivateKey(privateKeyPassword))
+    print("\nPublic key:")
+    print(wallet.calculatePublicKey())
